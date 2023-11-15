5df8972bf25efedb495281d441fd46b182e63e91
Beomsu <gobeumsu@gmail.com>
2023-11-15 16:06:45 +0900

    feat : .gitignore

---
+++ src/main/java/com/jnu/wifi6/Wifi6Application.java
@@ -0,0 +1,13 @@
+package com.jnu.wifi6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wifi6Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Wifi6Application.class, args);
+  }
+
+}
