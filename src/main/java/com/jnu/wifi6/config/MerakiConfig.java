95071c22c51f629e91742bd05788ada6c147c699
Beomsu <gobeumsu@gmail.com>
2023-11-20 15:50:04 +0900

    fix : meraki 의존성 주입 typo

--- src/main/java/com/jnu/wifi6/config/MerakiConfig.java
+++ src/main/java/com/jnu/wifi6/config/MerakiConfig.java
@@ -1,17 +1,22 @@
 package com.jnu.wifi6.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-
+@Getter
+@Component
 public class MerakiConfig {
+
+  private final String baseUrl = "https://api.meraki.com/api/v1";
 
   @Value("${meraki.organizationId}")
   private String organizationId;
 
-  @Value("{meraki.apiKey}")
+  @Value("${meraki.apiKey}")
   private String apiKey;
 
-  @Value("{meraki.networkId}")
+  @Value("${meraki.networkId}")
   private String networkId;
 
 }
