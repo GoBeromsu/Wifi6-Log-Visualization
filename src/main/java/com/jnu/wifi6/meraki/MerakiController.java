97be225277133005e4b1fb3ea8700b2f9fa70ed4
Beomsu <gobeumsu@gmail.com>
2023-11-20 15:50:25 +0900

    feat : /meraki/client

--- src/main/java/com/jnu/wifi6/meraki/MerakiController.java
+++ src/main/java/com/jnu/wifi6/meraki/MerakiController.java
@@ -1,7 +1,10 @@
 package com.jnu.wifi6.meraki;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jnu.wifi6.meraki.usecase.GetClientCountUsecase;
 import com.jnu.wifi6.meraki.usecase.GetNetworkUsageUsecase;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +22,8 @@ public class MerakiController {
   }
 
   @GetMapping("/meraki/client")
-  public void getClient() {
-    getNetworkUsageUsecase.execute();
+  public void getClient() throws JsonProcessingException {
+    List<Map<String, Object>> data = getNetworkUsageUsecase.execute();
+    System.out.println(data);
   }
 }
--- src/main/java/com/jnu/wifi6/meraki/usecase/GetNetworkUsageUsecase.java
+++ src/main/java/com/jnu/wifi6/meraki/usecase/GetNetworkUsageUsecase.java
@@ -1,10 +1,73 @@
 package com.jnu.wifi6.meraki.usecase;
 
-import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnu.wifi6.config.MerakiConfig;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class GetNetworkUsageUsecase {
 
-  public void execute() {
+  private final MerakiConfig merakiConfig;
+
+  private static final String API_KEY = "X-Cisco-Meraki-API-Key";
+
+  public List<Map<String, Object>> execute() throws JsonProcessingException {
+    String baseUrl = merakiConfig.getBaseUrl();
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.set(API_KEY, merakiConfig.getApiKey());
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    String startDate = "2023-10-25T00:00:00Z";
+
+    RestTemplate restTemplate = new RestTemplate();
+    List<Map<String, Object>> allData = new ArrayList<>();
+
+    String url = String.format("%s/networks/%s/clients?perPage=1000&t0=%s", baseUrl,
+        merakiConfig.getNetworkId(), startDate);
+    while (url != null && !url.isEmpty()) {
+      HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+      ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity,
+          String.class);
+
+      if (response.getStatusCodeValue() != 200) {
+        System.out.println("API 요청에 실패했습니다. 상태 코드: " + response.getStatusCode());
+        System.out.println("응답 메시지: " + response.getBody());
+        break;
+      }
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      List<Map<String, Object>> data = objectMapper.readValue(response.getBody(),
+          new TypeReference<List<Map<String, Object>>>() {
+          });
+      allData.addAll(data);
+
+      url = extractNextLink(response.getHeaders().get("Link"));
+    }
+
+    return allData;
+  }
+
+  private static String extractNextLink(List<String> linkHeader) {
+    if (linkHeader != null) {
+      for (String link : linkHeader) {
+        if (link.contains("rel=\"next\"")) {
+          return link.substring(link.indexOf('<') + 1, link.indexOf('>'));
+        }
+      }
+    }
+    return null;
   }
 }
