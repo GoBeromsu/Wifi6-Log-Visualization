ee8795c555d6f95c2d18af292c2d96e67c06a739
Beomsu <gobeumsu@gmail.com>
2023-12-20 16:02:47 +0900

    feat : InfluxDB Config 설정

--- src/main/java/com/jnu/wifi6/config/InfluxDBConfig.java
+++ src/main/java/com/jnu/wifi6/config/InfluxDBConfig.java
@@ -1,13 +1,16 @@
 package com.jnu.wifi6.config;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxDBClientFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InfluxDBConfig {
 
   @Value("${influxdb.url}")
-  private String serverURL;
+  private String influxDBUrl;
 
   @Value("${influxdb.username}")
   private String username;
@@ -15,9 +18,13 @@ public class InfluxDBConfig {
   @Value("${influxdb.password}")
   private String password;
 
-  @Value("${influxdb.database}")
-  private String database;
+  @Value("${influxdb.bucket}")
+  private String bucket;
+  @Value("${influxdb.token}")
+  private String token;
 
-  @Value("${influxdb.retentionPolicy}")
-  private String retentionPolicy;
+  @Bean
+  public InfluxDBClient influxDBClient() {
+    return InfluxDBClientFactory.create(influxDBUrl, token.toCharArray());
+  }
 }
