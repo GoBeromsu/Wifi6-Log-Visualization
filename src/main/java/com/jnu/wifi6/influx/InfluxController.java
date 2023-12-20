a8ced905abe716775090f1d650e7114d0efa38a0
Beomsu <gobeumsu@gmail.com>
2023-12-20 18:26:05 +0900

    fix : 엔드포인트 오류

--- src/main/java/com/jnu/wifi6/influx/InfluxController.java
+++ src/main/java/com/jnu/wifi6/influx/InfluxController.java
@@ -2,20 +2,17 @@ package com.jnu.wifi6.influx;
 
 import com.jnu.wifi6.influx.usecase.InsertInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/influx")
 public class InfluxController {
 
   private final InsertInfo insertInfo;
 
 
-  @PostMapping("/")
+  @PostMapping("/influx")
   public void insertInfo(Long predictedValue) {
     insertInfo.execute(predictedValue);
   }
