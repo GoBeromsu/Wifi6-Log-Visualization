0b519495925efc236ad8f98598af9218f0b4c726
Beomsu <gobeumsu@gmail.com>
2024-01-02 17:57:58 +0900

    refactor: api 요청 시간대 수정
    - 한국 시간대로 변경 (메라키 대시보드가 한국 시간대)
    - 00:00 부터의 데이터를 가져오도록 변경

--- src/main/java/com/jnu/wifi6/meraki/command/GetMerakiInfo.java
+++ src/main/java/com/jnu/wifi6/meraki/command/GetMerakiInfo.java
@@ -3,7 +3,7 @@ package com.jnu.wifi6.meraki.command;
 import com.jnu.wifi6.influx.command.RestClient;
 import com.jnu.wifi6.meraki.dto.GetMerakiDTO;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,16 +21,20 @@ public class GetMerakiInfo {
 
   public GetMerakiDTO execute() {
     String currentDate = getCurrentDateTime();
-    GetMerakiDTO merakiData = getMerakiClient.execute(currentDate, 1000000000, 300);
+    GetMerakiDTO merakiData = getMerakiClient.execute(currentDate, 1000000000, 3000);
 
     // POST 요청 보내기
-    restClient.sendPostRequest(flaskServerUrl, merakiData, Void.class); // Void.class는 응답 타입을 나타냅니다.
+    restClient.sendPostRequest(flaskServerUrl, merakiData, Void.class);
     return merakiData;
 
   }
 
   private String getCurrentDateTime() {
-    LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
-    return now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "Z";
+    // 한국 시간대 설정
+    ZoneId koreaZoneId = ZoneId.of("Asia/Seoul");
+    // 오늘 날짜에 해당하는 자정 시간 설정
+    LocalDateTime midnight = LocalDateTime.now(koreaZoneId).toLocalDate().atStartOfDay();
+    return midnight.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "Z";
   }
+
 }
